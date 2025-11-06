7b8988b9b69039876b6d8a123e943190276ac99d
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-06 15:47:50 +0100

    refactor: change the variable names to not conflict with environment variables

--- edge-pwa-grouping-patch.sh
+++ edge-pwa-grouping-patch.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-DESKTOP_DIR="${XDG_DATA_HOME}/applications"
-PROFILE_NAME="Default"
+desktop_dir="${XDG_DATA_HOME}/applications"
+profile_name="Default"
 
 # Find and loopo trough all matching .desktop files
-find "${DESKTOP_DIR}" -type f -name "msedge-*-${PROFILE_NAME}.desktop" | while read -r file; do
+find "${desktop_dir}" -type f -name "msedge-*-${profile_name}.desktop" | while read -r file; do
     echo "Processing: ${file}"
 
-    id=$(basename "${file}" | sed -E "s/msedge-(.*)-${PROFILE_NAME}\.desktop/\1/")
+    id=$(basename "${file}" | sed -E "s/msedge-(.*)-${profile_name}\.desktop/\1/")
 
     # Replace StartupWMClass with the correct value (msedge-_<id>-<profile name>)
-    wmclass_value="msedge-_${id}-${PROFILE_NAME}"
+    wmclass_value="msedge-_${id}-${profile_name}"
     if grep -q '^StartupWMClass=' "${file}"; then
         sed -i "s/^StartupWMClass=.*/StartupWMClass=${wmclass_value}/" "${file}"
     else
@@ -23,7 +23,7 @@ find "${DESKTOP_DIR}" -type f -name "msedge-*-${PROFILE_NAME}.desktop" | while r
     echo "X-GNOME-SingleWindow=true" >> "${file}"
 
     # Rename the file
-    new_name="${DESKTOP_DIR}/com.microsoft.${id}.desktop"
+    new_name="${desktop_dir}/com.microsoft.${id}.desktop"
     mv "${file}" "${new_name}"
     echo "Renamed to: ${new_name}"
 done
